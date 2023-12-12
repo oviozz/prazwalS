4ae5d0e0270f14a89c1194dcc10c3f2bef8988ad
prajwal <42685801+oviozz@users.noreply.github.com>
2023-12-12 10:06:12 -0800

    Update HomeTopDisplay.jsx
    Update: profile pic src

--- src/Components/PagesComp/Home/TopDisplay/HomeTopDisplay.jsx
+++ src/Components/PagesComp/Home/TopDisplay/HomeTopDisplay.jsx
@@ -5,6 +5,8 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { HiOutlineArrowDown } from "react-icons/hi2";
 import Badge from '@mui/joy/Badge';
+import profilePic from '../Components/Assests/profilePic.jpg'
+
 
 
 function HomeTopDisplay() {
@@ -16,7 +18,7 @@ function HomeTopDisplay() {
 
                 <div className={"relative inline-block"}>
 
-                    <img src={"./Components/Assests/profilePic.png"} className={'rounded-full border-[4px] border-white drop-shadow-2xl  animate-fade-up'} width={'100'}/>
+                    <img src={profilePic} className={'rounded-full border-[4px] border-white drop-shadow-2xl  animate-fade-up'} width={'100'}/>
 
                     <span
                         className="absolute translate-x-1/2 left-auto top-0 right-0 inline-flex items-center bg-purple-100 text-purple-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
